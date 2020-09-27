3f6293e48388b01169a3a9584b48cb41b492d880
Leonardo Vinicius Florencio Monteiro <60299840+Lporchart@users.noreply.github.com>
2020-09-27 02:39:30 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 const modaloverlay = document.querySelector('.modal-overlay');
+const modal = document.querySelector('.modal');
 const cards = document.querySelectorAll('.card');
 
 
@@ -6,12 +7,17 @@ for(let card of cards){
     card.addEventListener('click', function(){
         modaloverlay.classList.add('active')
         id = card.id;
-        modaloverlay.querySelector("iframe").src = `https://www.youtube.com/embed/${id}`;         
+        modaloverlay.querySelector("iframe").src = `${id}`;         
     });
 }
 
+
 document.querySelector('.close-modal').addEventListener("click", function(){
     modaloverlay.classList.remove('active') 
+    modal.classList.remove('active') 
     modaloverlay.querySelector("iframe").src = "";
-})
+});
+document.querySelector('.maximize-modal').addEventListener("click", function(){
+    modal.classList.add('active')
+});
 
